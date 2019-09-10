34c6dad41a865bb632375ed39a2050b1e94a8006
Alex Perkins <alex.jakob.perkins@gmail.com>
2019-09-09 17:44:43 -0700

    Got the skeleton structure of the HTML added, created some minimal CSS styling for readbility's sake, and started defining a few basic variables in the .js document

---
+++ javascript/game.js
@@ -0,0 +1,8 @@
+
+// Initial base of code 
+var losses = document.getElementById("losses")
+var wins = document.getElementById("wins")
+var guessesLeft = document.getElementById("guess-left")
+var guessesSoFar = document.getElementById("guesses-so-far")
+
+document.onclick(prompt)
