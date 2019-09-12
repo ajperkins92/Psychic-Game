4efd9d6711981e5f435d73ed1d66dac7f3021bee
Alex Perkins <alex.jakob.perkins@gmail.com>
2019-09-12 01:09:19 -0700

    Was able to modify the javascript, and it is now functioning as intended

--- javascript/game.js
+++ javascript/game.js
@@ -1,5 +1,5 @@
 
-// Initial base of code to define variables
+// Variables that define the starting number/letter values of the game
 var losses = 0;
 var wins = 0;
 var guessesLeft = 10;
@@ -7,64 +7,57 @@ var guessesSoFar = [];
 var letterChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
 
+// Variables that will be used to tie the value-holding varialbes above to the HTML elements on the page
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
-var guessesLeftText = document.getElementById("guess-left-text"); 
+var guessesLeftText = document.getElementById("guess-left-text");
 var guessesSoFarText = document.getElementById("guesses-so-far-text");
 
 
 // A bunch of alerts describing the conditions of the game
 
-//    alert("Welcome to the Psychic Game!");
+alert("Welcome to the Psychic Game!");
 
-//    alert("The objective of the game is to demonstrate your psychic prowess by guessing the letter that I'm thinking of!");
+alert("The objective of the game is to demonstrate your psychic prowess by guessing the letter that I'm thinking of!");
 
-//    alert("If you should happen to guess the correct letter that I'm thinking of within 9 tries, you will win the round!");
+alert("If you should happen to guess the correct letter that I'm thinking of within 9 tries, you will win the round!");
 
-//    alert("If, however, you fail to guess correctly, I will win the round!");
+alert("If, however, you fail to guess correctly, I will win the round!");
 
 alert("LET THE GAMES BEGIN!!!");
 
-// This is where the actual game will get played
+// The game actually starts getting played here
 document.onkeyup = function (event) {
 
     var userGuess = event.key;
-    guessesSoFar.push(userGuess);
+    // guessesSoFar.push(userGuess);
     var computerGuess = letterChoices[Math.floor(Math.random() * letterChoices.length)];
 
-    if (computerGuess.includes(userGuess)) {
-        if (userGuess == computerGuess) {
-            wins++;
-            guessesSoFar.push(userGuess);
-            guessesLeft--;
-        }
+    if (userGuess === computerGuess) {
+        wins++;
+        guessesSoFar = [];
+        guessesLeft = 10;
+    }
 
-        if (userGuess !== computerGuess) {
-            guessesSoFar.push(userGuess);
-            guessesLeft--;
-        }
-        if (chances === 0) {
-            losses++;
-            guessesSoFar = [];
-        }
-
-        winsText.textContent = wins;
-        lossesText.textContent = losses;
-        guessesLeftText.textContent = guessesLeft;
-        guessesSoFarText.textContent = guessesSoFar;
+    if (userGuess !== computerGuess) {
+        guessesSoFar.push(userGuess);
+        guessesLeft--;
+    }
+    if (guessesLeft === 0) {
+        losses++;
+        guessesSoFar = [];
+        guessesLeft = 10;
     }
 
 
 
-
-
-    var html =
-        '<p><h1>The Psychic Game</h1></p>' +
-        '<p>Guess what letter I am thinking of</p>' +
-        '<p>Wins: ' + wins + '</p>' +
-        '<p>Losses: ' + losses + '</p>' +
-        '<p>Guesses Left: ' + guessesLeft + '</p>' +
-        '<p>Your Guesses: ' + guessesSoFar + '</p>';
-    document.getElementById("game").innerHTML = html;
+    winsText.textContent = wins;
+    lossesText.textContent = losses;
+    guessesLeftText.textContent = guessesLeft;
+    guessesSoFarText.textContent = guessesSoFar;
 };
+
+
+
+
 
